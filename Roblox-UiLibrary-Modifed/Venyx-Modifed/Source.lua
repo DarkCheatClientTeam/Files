3a8b7f423dcfc4201f80b99ad261f4d0a15b49b0
Gregory909 <124597526+NecroxisTeam@users.noreply.github.com>
2024-11-23 23:27:21 +0500

    Update Source.lua

--- Roblox-UiLibrary-Modifed/Venyx-Modifed/Source.lua
+++ Roblox-UiLibrary-Modifed/Venyx-Modifed/Source.lua
@@ -919,10 +919,6 @@ do
 			return section:updateToggle(module)
 		end
 
-		if data.default == true then
-		     this.callback(this.toggled)
-		end
-
 		toggle.MouseButton1Click:Connect(function()
 			this.toggled = not this.toggled
 			self:updateToggle(module)
