f67db2bf044c7171d98edc5f592642ed4e907837
Gregory909 <124597526+NecroxisTeam@users.noreply.github.com>
2024-11-23 10:37:21 +0500

    Update JKQGDUAJQV.lua

--- JKQGDUAJQV.lua
+++ JKQGDUAJQV.lua
@@ -1,9 +1,25 @@
 local a = {}
 local b = {1993687031, 1293792120} 
+local b1 = {}
+local b2 = {}
 local c = {3844044411, 2827021146, 7426644606} 
 
 function a:isb(p) 
   if table.find(b, p.UserId) then
+    return true
+  else
+    return false
+end
+end
+function a:isb1(string) 
+  if table.find(b1, string) then
+    return true
+  else
+    return false
+end
+end
+function a:isb2(string) 
+  if table.find(b2, string) then
     return true
   else
     return false
