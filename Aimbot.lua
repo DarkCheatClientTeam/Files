86c1adbb0909c862e2bb95669258175b431892a2
Gregory909 <124597526+DarkCheatClientTeam@users.noreply.github.com>
2025-10-30 11:15:50 +0300

    Delete Aimbot.lua

--- Aimbot.lua
+++
@@ -1,2 +0,0 @@
-setclipboard("Discord Serverv Link: https://discord.gg/ugaTf7Cs6t")
-game.Players.LocalPlayer:Kick("Script Got Disconnected! Reason You Can See In Discord Server(copied to clipboard)!")
